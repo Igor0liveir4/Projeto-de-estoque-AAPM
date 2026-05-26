080c40c46c4b809d05af6653cbfc5478744d7839
igor <igoroliveirapupo3@gmail.com>
2026-05-26 13:30:21 -0300

    adiciona controladores de admin, categoria e produto; cria tabelas de categorias e produtos

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
+# Gerar as migrations
+# python -m alembic revision --autogenerate -m "Criando tabelas de categorias e produtos"
+# python -m alembic upgrade head
+
 from app.models import categoria
 from app.models  import produto
 from app.models import usuario
